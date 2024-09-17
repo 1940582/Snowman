2dbe5702a3fa8fdd3a1f1adc69d007488770b498
1940582 <chrisbjin@gmail.com>
2024-09-17 11:10:36 -0700

    Add files via upload

---
+++ Snowman Project.py
@@ -0,0 +1,24 @@
+import turtle as trtl
+wn = trtl.Screen()
+wn.setup(width = 500, height = 400)
+wn.bgpic("H:\Chrome Downloads\christmas-winter-background-snow-christmas-winter-background-snow-blurred-bokeh-merry-christmas-happ.png")
+
+trtl.penup()
+trtl.goto(0, -150)
+trtl.pendown()
+trtl.circle(50)
+
+trtl.penup()
+trtl.goto(0, -50)
+trtl.pendown()
+trtl.circle(35)
+
+trtl.penup()
+trtl.goto(0, 20)
+trtl.pendown()
+trtl.circle(20)
+
+ 
+    
+
+wn.mainloop()
